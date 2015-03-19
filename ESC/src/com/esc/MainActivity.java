f88aea348a684533672c20b7d7f9bd0f3dee3907
frogSM <frogSM@frogSM-PC>
2015-03-20 03:24:10 +0900

    욱현 작업환경 설정 	- MainActivity 시리얼포트 주석 (상현) 			----> 주석해제 	- SocketHelper serverAddress 192.168.25.2 (집 내부 아이피) 	        ----> 본래 공인아이피 58.231.74.66 설정

--- ESC/src/com/esc/MainActivity.java
+++ ESC/src/com/esc/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends FragmentActivity {
 		mFragmentTransaction.commit();
 		
 		productManager = new ProductManager(this);
-//		productManager.OpenSerialPort();
+		productManager.OpenSerialPort();
 	}
 	
 	@Override
