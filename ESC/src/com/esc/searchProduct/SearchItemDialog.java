41f2a322f3c166d19cc041a25fd18ff6a97a6ad9
frogSM <frogSM@frogSM-PC>
2015-04-27 17:22:13 +0900

    상품정보 다이얼로그에 그래프 추가  - 그래프만 동일하게 추가시켜놓음. 각 상품에 대한 데이터 구현 X

--- ESC/src/com/esc/searchProduct/SearchItemDialog.java
+++ ESC/src/com/esc/searchProduct/SearchItemDialog.java
@@ -1,10 +1,20 @@
 package com.esc.searchProduct;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.BarChart.Type;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.SimpleSeriesRenderer;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
 
 import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Paint.Align;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -24,6 +34,7 @@ import android.widget.ViewFlipper;
 import com.esc.R;
 import com.esc.productManager.Product;
 import com.esc.shoppingBasket.BasketManager;
+
 
 public class SearchItemDialog extends Dialog implements OnClickListener{
 
@@ -87,17 +98,117 @@ public class SearchItemDialog extends Dialog implements OnClickListener{
 		mLayoutInflater = (LayoutInflater)mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
 	}
 	
+	private GraphicalView getGraph(String priceBeforeOne, String priceBeforeTwo, 
+			String priceBeforeThree, String priceBeforeSix) {
+		List<double[]> values = new ArrayList<double[]>();
+		values.add(new double[] { 14230, 12300, 14240, 15244, 15900, 19200,
+				22030, 21200, 19500, 15500, 12600, 14000 });
+
+		/** 그래프 출력을 위한 그래픽 속성 지정객체 */
+		XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+
+		// 상단 표시 제목과 글자 크기
+		renderer.setChartTitle("2011년도 판매량");
+		renderer.setChartTitleTextSize(20);
+
+		// 분류에 대한 이름
+		String[] titles = new String[] { "월별 판매량" };
+
+		// 항목을 표시하는데 사용될 색상값
+		int[] colors = new int[] { Color.RED };
+
+		// 분류명 글자 크기 및 각 색상 지정
+		renderer.setLegendTextSize(15);
+		int length = colors.length;
+		for (int i = 0; i < length; i++) {
+			SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+			r.setColor(colors[i]);
+			r.setDisplayChartValues(true);
+			renderer.addSeriesRenderer(r);
+		}
+
+		// X,Y축 항목이름과 글자 크기
+		renderer.setXTitle("월");
+		renderer.setYTitle("판매량");
+		renderer.setAxisTitleTextSize(12);
+
+		// 수치값 글자 크기 / X축 최소,최대값 / Y축 최소,최대값
+		renderer.setLabelsTextSize(10);
+		renderer.setXAxisMin(0.5);
+		renderer.setXAxisMax(12.5);
+		renderer.setYAxisMin(0);
+		renderer.setYAxisMax(24000);
+
+		// X,Y축 라인 색상
+		renderer.setAxesColor(Color.WHITE);
+		// 상단제목, X,Y축 제목, 수치값의 글자 색상
+		renderer.setLabelsColor(Color.RED);
+
+		// X축의 표시 간격
+		renderer.setXLabels(12);
+		// Y축의 표시 간격
+		renderer.setYLabels(5);
+
+		// X,Y축 정렬방향
+		renderer.setXLabelsAlign(Align.LEFT);
+		renderer.setYLabelsAlign(Align.LEFT);
+		// X,Y축 스크롤 여부 ON/OFF
+		renderer.setPanEnabled(false, false);
+		// ZOOM기능 ON/OFF
+		renderer.setZoomEnabled(false, false);
+		// ZOOM 비율
+		renderer.setZoomRate(1.0f);
+		// 막대간 간격
+		renderer.setBarSpacing(0.5f);
+		
+		// 그리드 색상 변경 및 마진 컬러 변경
+		renderer.setGridColor(Color.parseColor("#c9c9c9"));
+        renderer.setMarginsColor(Color.parseColor("#FFFFFF"));
+
+		// 설정 정보 설정
+		XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+		for (int i = 0; i < titles.length; i++) {
+			CategorySeries series = new CategorySeries(titles[i]);
+			double[] v = values.get(i);
+			int seriesLength = v.length;
+			for (int k = 0; k < seriesLength; k++) {
+				series.add(v[k]);
+			}
+			dataset.addSeries(series.toXYSeries());
+		}
+		
+		// 그래프 객체 생성
+		GraphicalView gv = ChartFactory.getBarChartView(mContext, dataset,
+				renderer, Type.STACKED);
+		
+		return gv;
+
+//		// 그래프를 LinearLayout에 추가
+//		LinearLayout llBody = (LinearLayout)ll.findViewById(R.id.ll_graph);
+//		llBody.addView(gv);
+	}
+	
 	private void addDynamicViewFlipper() {
 		for(int i=0 ; i<receiveData.size() ; i++) {
 			LinearLayout ll = (LinearLayout)mLayoutInflater.inflate(R.layout.item_productinfo, null);
 			TextView mTextViewName= (TextView)ll.findViewById(R.id.tv_name);
 			TextView mTextViewPrice= (TextView)ll.findViewById(R.id.tv_price);
+			TextView mTextViewType = (TextView)ll.findViewById(R.id.tv_type);
+			TextView mTextViewManufacturer = (TextView)ll.findViewById(R.id.tv_manufacturer);
+			TextView mTextViewDescription = (TextView)ll.findViewById(R.id.tv_description);
 			ImageView mImageView = (ImageView)ll.findViewById(R.id.iv_productpicture);
 			Button mButtonBasketAdd = (Button)ll.findViewById(R.id.btn_basket_add);
 			Button mButtonPositionConfirm = (Button)ll.findViewById(R.id.btn_position_confirm);
+			LinearLayout mGraph = (LinearLayout)ll.findViewById(R.id.ll_graph);
 			
 			mTextViewName.setText(receiveData.get(i).getName());
 			mTextViewPrice.setText(String.valueOf(receiveData.get(i).getPriceNow()));
+			mTextViewType.setText(receiveData.get(i).getType());
+			mTextViewManufacturer.setText(receiveData.get(i).getManufacturer());
+			mTextViewDescription.setText(receiveData.get(i).getDescription());
+			
+			mGraph.addView(getGraph(receiveData.get(i).getPriceBeforeOne(), receiveData.get(i).getPriceBeforeTwo(),
+					receiveData.get(i).getPriceBeforeThree(), receiveData.get(i).getPriceBeforeSix()));
 			
 			int id = mContext.getResources().getIdentifier(receiveData.get(i).getImgURL(), "drawable", mContext.getPackageName());
 			mImageView.setImageResource(id);
